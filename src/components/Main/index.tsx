20f2193555be7507f931cd83659ac6e61d260905
Flavio Ever <flavio.ever@icloud.com>
2021-10-11 22:07:24 -0300

    style test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,13 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React teste</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="" />
+    <S.Title>React Teste</S.Title>
+    <S.Description>
+      Bacon ipsum dolor amet fatback boudin kevin shankle tri-tip meatloaf.
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
