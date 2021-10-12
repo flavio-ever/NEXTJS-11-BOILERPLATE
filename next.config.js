7dfaa212b075cf3eecd4720fe353b81c7379cbbc
Flavio Ever <flavio.ever@icloud.com>
2021-10-11 23:27:10 -0300

    add config. pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-// eslint-disable-next-line no-undef
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>React Teste - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-192.png" />
         <link rel="apple-touch-icon icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
         <meta name="description" content="A simple project" />
       </Head>
       <GlobalStyles />
